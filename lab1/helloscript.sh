bfa121ee180a23357fc8316b5d120b88c40fd9a3
Dwayne <dwayne@debian.localdomain>
2017-01-20 14:36:45 -0500

    Moved files to lab1

---
+++ lab1/helloscript.sh
@@ -0,0 +1,6 @@
+# helloscript.sh
+# This script displays the string "Hello World!"
+
+# This is a silly way of creating the output text by starting with something else and stream editing it into a pipeline
+echo -n  "helb wold" |sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk '{print $1 "\x20" $2 "\41"}'
+
