5acdc6cad99abbbbd1218b43fca95e7f52889f14
root <root@debian.localdomain>
2017-02-03 14:55:26 -0500

    Updated

--- lab2/updategit.sh
+++ lab2/updategit.sh
@@ -5,6 +5,7 @@ git add -A
 
 # $1 grabs the first entry after the command, $@ grabs all entries after the command
 # enclosing $1 in quotes ensures everything is grabbed, including spaces
+
 git commit -m "$1"
 
 git push
