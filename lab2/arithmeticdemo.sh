9fca58cfa2ec8c813aeca2816a22af20ff698e8a
root <root@debian.localdomain>
2017-02-02 22:30:06 -0500

    updated files in lab2

--- lab2/arithmeticdemo.sh
+++ lab2/arithmeticdemo.sh
@@ -2,6 +2,7 @@
 # This script prompts the user for 2 numbers. It then performs the 5 different arithmetic funtions on the number.
 # Finally, it displays the results in a user-friendly way.
 
+echo
 read -p "Give me a number please: " num1
 echo
 echo "You gave me $num1"
@@ -11,17 +12,17 @@ echo
 echo "You gave me $num2"
 echo
 numsum=$((num1 + num2))
-echo "The result of adding the 2 numbers is: $numsum"
+echo "The result of $num1 + $num2 is $numsum"
 echo
 numdiff=$((num1 - num2))
-echo "The result of subtracting the second number from the first number is: $numdiff"
+echo "The result of $num1 - $num2 is $numdiff"
 echo
 nummultiply=$((num1 * num2))
-echo "The result of multiplying the 2 numbers is: $nummultiply"
+echo "The result of $num1 * $num2 is $nummultiply"
 echo
 numdivide=$((num1 / num2))
-echo "The result of dividing the first number by the second numer is: $numdivide"
+echo "The result of $num1 / $num2 is $numdivide"
 echo
 numremain=$((num1 % num2))
-echo "The remainder from dividing the first number by the second number is: $numremain"
+echo "The division remainder is $numremain"
 echo
